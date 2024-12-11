fdc85b36422e0a357d8b7446f3d828af2bb52c44
KutayMumcu <kutaymumcu@gmail.com>
2024-12-11 19:47:50 +0300

    message

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Hotel hotel = new Hotel();
         HotelView view = new HotelView();
-
+        System.out.println("Hello World!");
         new HotelManager(hotel, view);
     }
 }
