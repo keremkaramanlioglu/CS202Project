889d5d05bccc4a3e3d9965e3b87a90d3d91d7356
keremkaramanlioglu <kerem.karamnlioglu@ozu.edu.tr>
2024-12-12 12:06:51 +0300

    demo db connection system test added.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import entities.*;
 import panels.adminPanels.EmployeesPanel;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -11,21 +12,42 @@ public class Main {
         try {
             String tableName = "Rooms";
             String sql = "select * from " + tableName;
-            String url = "jdbc:mysql://localhost:3306/myDB";
-            String userName = "root";
-            String password = "kerem1453";
+            String userName;
+            String password;
+            String port;
+            String hostName;
+            String dbName;
 
+            Scanner s = new Scanner(System.in);
+            System.out.print("Enter the username: ");
+            userName = s.nextLine();
+            System.out.print("Enter the password: ");
+            password = s.nextLine();
+            System.out.print("Enter the port: ");
+            port = s.nextLine();
+            System.out.print("Enter the hostname: ");
+            hostName = s.nextLine();
+            System.out.print("Enter the db name: ");
+            dbName = s.nextLine();
+            System.out.println("Connecting to database...");
+
+            String url = "jdbc:mysql://" + hostName + ":" + port + "/" + dbName;
 
             Connection conn = DriverManager.getConnection(url, userName, password);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-            while(rs.next()) {
-                if (rs.isClosed()) break;
-                Room r = new Room(rs);
-                System.out.println(r);
+
+            if (conn != null && conn.isValid(0)) {
+                System.out.println("Database " + dbName + " is connected successfully.");
             }
+
+//            Statement stmt = conn.createStatement();
+//            ResultSet rs = stmt.executeQuery(sql);
+//            while(rs.next()) {
+//                if (rs.isClosed()) break;
+//                Room r = new Room(rs);
+//                System.out.println(r);
+//            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 }
