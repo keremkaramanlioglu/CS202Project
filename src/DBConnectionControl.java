dc8d5e4b54d899b189213002755240d643fb3679
KutayMumcu <kutaymumcu@gmail.com>
2024-12-14 18:36:31 +0300

    DBConnectionControl has been updated

--- src/DBConnectionControl.java
+++ src/DBConnectionControl.java
@@ -1,4 +1,30 @@
+import java.sql.*;
+
 public class DBConnectionControl {
     String hostName;
     String dbName;
+    String userName;
+    String password;
+    String port;
+    public DBConnectionControl(String hostName, String dbName, String userName, String password, String port) throws SQLException {
+        this.hostName = hostName;
+        this.dbName = dbName;
+        this.userName = userName;
+        this.password = password;
+        this.port = port;
+        try {
+            createConnection();
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void createConnection() throws SQLException {
+        String url = "jdbc:mysql://" + hostName + ":" + port + "/" + dbName;
+        Connection conn = DriverManager.getConnection(url, userName, password);
+        if (conn != null && conn.isValid(0)) {
+            System.out.println("Database " + dbName + " is connected successfully.");
+        }
+    }
 }
