dcd2e14fb2b05bef85ac0ab12e430e4d5fc82a8d
keremkaramanlioglu <kerem.karamnlioglu@ozu.edu.tr>
2024-12-16 14:20:09 +0300

    last version

--- src/panels/Panel.java
+++ src/panels/Panel.java
@@ -7,12 +7,6 @@ public abstract class Panel extends JPanel {
     public abstract void addButtonListener(ActionListener al);
     public abstract void setSelectedButton(JButton b);
     public abstract void reset();
-<<<<<<< HEAD
     public abstract void setCenterPanel(Panel panel);
     public abstract Panel getPanelByName(String panelName);
-}//shduoawhd
-=======
-    public abstract void setCenterPanel(JPanel panel);
-    public abstract JPanel getPanelByName(String panelName);
 }
->>>>>>> ad1d9581c527bdcc54f455edb6a1381d2215fcff
