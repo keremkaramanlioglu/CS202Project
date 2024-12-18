3fe27a707fccc73f39c91d15ba7e3ffb47b6f294
keremkaramanlioglu <kerem.karamnlioglu@ozu.edu.tr>
2024-12-18 21:45:33 +0300

    m

--- src/panels/managerPanels/EmployeesPanel.java
+++ src/panels/managerPanels/EmployeesPanel.java
@@ -43,7 +43,7 @@ public class EmployeesPanel extends Panel {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
-        // Generated using JFormDesigner Evaluation license - Kutay Mumcu
+        // Generated using JFormDesigner Evaluation license - Kerem Karamanlıoğlu
         pnlData = new JScrollPane();
         tblEmployees = new JTable();
         pnlControl = new JPanel();
@@ -63,6 +63,7 @@ public class EmployeesPanel extends Panel {
         btnApplyFilter = new JButton();
 
         //======== this ========
+<<<<<<< Updated upstream
         setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new
         javax. swing. border. EmptyBorder( 0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax
         . swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java
@@ -70,6 +71,15 @@ public class EmployeesPanel extends Panel {
         . Color. red) , getBorder( )) );  addPropertyChangeListener (new java. beans.
         PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062ord\u0065r" .
         equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
+=======
+        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new
+        javax . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JFor\u006dDesi\u0067ner \u0045valu\u0061tion" , javax
+        . swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java
+        . awt .Font ( "Dia\u006cog", java .awt . Font. BOLD ,12 ) ,java . awt
+        . Color .red ) , getBorder () ) );  addPropertyChangeListener( new java. beans .
+        PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e) { if( "bord\u0065r" .
+        equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
+>>>>>>> Stashed changes
         setLayout(new BorderLayout());
 
         //======== pnlData ========
@@ -79,11 +89,11 @@ public class EmployeesPanel extends Panel {
             //---- tblEmployees ----
             tblEmployees.setModel(new DefaultTableModel(
                 new Object[][] {
-                    {null, null, null, null, null, null, null, null, null, null, null, null, null, null, null},
+                    {null, null, null, null, null, "", null, null, null, "", null, null, null, null, null},
                     {null, null, null, null, null, null, null, null, null, null, null, null, null, null, null},
                 },
                 new String[] {
-                    "Ssn", "First Name", "Last Name", "Type", "Birth Date", "Years", "Hotel ID", "Salary", "Phone No", "E-mail", "Gender", "Street", "No", "Apartment", "Zip Code"
+                    "ssn", "firstname", "lastname", "type", "bd", "years", "hotel_id", "salary", "phone_num", "email", "gender", "street", "no", "apartment", "zip_code"
                 }
             ) {
                 Class<?>[] columnTypes = new Class<?>[] {
@@ -260,7 +270,7 @@ public class EmployeesPanel extends Panel {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
-    // Generated using JFormDesigner Evaluation license - Kutay Mumcu
+    // Generated using JFormDesigner Evaluation license - Kerem Karamanlıoğlu
     private JScrollPane pnlData;
     private JTable tblEmployees;
     private JPanel pnlControl;
