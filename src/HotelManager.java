f92be1a5b7c6bc72c206ea594ad627b2e1e452f2
keremkaramanlioglu <kerem.karamnlioglu@ozu.edu.tr>
2024-12-18 17:51:55 +0300

    last working commit

--- src/HotelManager.java
+++ src/HotelManager.java
@@ -122,7 +122,7 @@ public class HotelManager {
                 if (activePanel.getCenterPanel() != null) activePanel.getCenterPanel().reset();
                 activePanel.setCenterPanel(activePanel.getPanelByName(command));
             } else if (compare(command, mainPanelOptions)) {
-                if (!checkAction(command)) return;
+                if (!command.equals("Customer")) if (!checkAction(command)) return;
                 hotelView.getActivePanel().reset();
                 hotelView.setActivePanel(hotelView.getPanelByName(command));
             } else {
