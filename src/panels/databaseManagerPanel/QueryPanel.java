ae8de78217e505e7f2cf05027e93ed9389da0baf
KutayMumcu <kutaymumcu@gmail.com>
2024-12-18 14:53:03 +0300

    unnecessary files deleted from DatabaseManager Panel and added a query panel

---
+++ src/panels/databaseManagerPanel/QueryPanel.java
@@ -0,0 +1,91 @@
+/*
+ * Created by JFormDesigner on Wed Dec 18 14:44:39 TRT 2024
+ */
+
+package panels.databaseManagerPanel;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author kutay
+ */
+public class QueryPanel extends JPanel {
+    public QueryPanel() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - Kutay Mumcu
+        pnlData = new JScrollPane();
+        tblData = new JTable();
+        pnlControl = new JPanel();
+        pnlQuery = new JScrollPane();
+        taQuery = new JTextArea();
+        btnExecute = new JButton();
+
+        //======== this ========
+        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .EmptyBorder (
+        0, 0 ,0 , 0) ,  "JF\u006frmDes\u0069gner \u0045valua\u0074ion" , javax. swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder
+        . BOTTOM, new java. awt .Font ( "D\u0069alog", java .awt . Font. BOLD ,12 ) ,java . awt. Color .
+        red ) , getBorder () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java .
+        beans. PropertyChangeEvent e) { if( "\u0062order" .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
+        setLayout(new BorderLayout());
+
+        //======== pnlData ========
+        {
+            pnlData.setViewportView(tblData);
+        }
+        add(pnlData, BorderLayout.CENTER);
+
+        //======== pnlControl ========
+        {
+            pnlControl.setPreferredSize(new Dimension(0, 200));
+            pnlControl.setLayout(null);
+
+            //======== pnlQuery ========
+            {
+
+                //---- taQuery ----
+                taQuery.setWrapStyleWord(true);
+                taQuery.setLineWrap(true);
+                pnlQuery.setViewportView(taQuery);
+            }
+            pnlControl.add(pnlQuery);
+            pnlQuery.setBounds(20, 15, 630, 170);
+
+            //---- btnExecute ----
+            btnExecute.setText("Execute");
+            pnlControl.add(btnExecute);
+            btnExecute.setBounds(670, 20, 130, 50);
+
+            {
+                // compute preferred size
+                Dimension preferredSize = new Dimension();
+                for(int i = 0; i < pnlControl.getComponentCount(); i++) {
+                    Rectangle bounds = pnlControl.getComponent(i).getBounds();
+                    preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                    preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+                }
+                Insets insets = pnlControl.getInsets();
+                preferredSize.width += insets.right;
+                preferredSize.height += insets.bottom;
+                pnlControl.setMinimumSize(preferredSize);
+                pnlControl.setPreferredSize(preferredSize);
+            }
+        }
+        add(pnlControl, BorderLayout.SOUTH);
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Kutay Mumcu
+    private JScrollPane pnlData;
+    private JTable tblData;
+    private JPanel pnlControl;
+    private JScrollPane pnlQuery;
+    private JTextArea taQuery;
+    private JButton btnExecute;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
